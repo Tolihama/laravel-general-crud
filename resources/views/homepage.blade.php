6c46076081b707446ef1c023d32e5500f6679ff6
Tolihama <fra070@gmail.com>
2022-01-28 13:40:07 +0100

    Project setup

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container-fluid homepage">
+        <div class="container">
+            Homepage
+        </div>
+    </div>
+    
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    FOOTER QUI
+</footer>
